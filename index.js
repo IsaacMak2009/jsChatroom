1c6d4e96e32256d12e7eec501755aaf59b94cd5a
IsaacMak2009 <1200024-8@g.puiching.edu.mo>
2022-07-09 23:56:27 +0800

    created public folder

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// TODO: finish this
+let express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send("Hello World!");
+});
+
+app.listen(5000);
+console.log*("listening on http://localhost:5000");
