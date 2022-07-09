07517f5c67da2f4d07eb07ce5371b56b866efc59
IsaacMak2009 <1200024-8@g.puiching.edu.mo>
2022-07-10 00:14:05 +0800

    New commit message.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-let express = require('express');
+// import libraries
+const express = require('express');
+const path = require('path');
 var app = express();
+
+// use ejs as view engine
+app.set('view engine', 'ejs');
+app.set("view", path.join(__dirname, 'public', 'html'));
+
+// set static path
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.send("Hello World!");
 });
 
 app.listen(5000);
-console.log*("listening on http://localhost:5000");
+console.log("listening on http://localhost:5000");
