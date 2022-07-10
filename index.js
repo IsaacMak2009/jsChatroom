438ea1c7773c2ef51b9b2be276abac71ede94abc
IsaacMak2009 <95964568+IsaacMak2009@users.noreply.github.com>
2022-07-10 23:33:34 +0800

    changed somethings

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // import libraries
 const express = require('express');
 const config = require('config');
+const bodyParser = require('body-parser');
 var app = express();
+
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 // load configuration
 var host = config.get('server.host');
 var port = config.get('server.port');
-var verify = config.get('app.verify');
-var verifycode = verify ? config.get('app.verify-code'):null;
 
+// list to store user names
+var userNames = [];
 
 // use ejs as view engine
 app.set("views", __dirname+'/public/');
@@ -19,10 +26,28 @@ app.set('view engine', 'html');
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.render('html/login.html', {
-        isverify: verify,
-        code: verifycode
-    });
+    if (req.query.err) {
+        res.render('html/login.html', {
+            isverify: verify,
+            code: verifycode,
+            errmsg: (req.query.err==undefined)?"":req.query.err
+    })};
+});
+
+app.post('/auth', function(req, res) {
+    var name =req.body.username;
+    if (!(userNames.includes(name))) {
+        userNames.push(name);
+        res.cookie('username',name);
+        res.redirect('/chatroom');
+        return;
+    } else {
+        res.redirect('/?err=username%20already%20used');
+    }
+});
+
+app.get('/chatroom', function(req, res){
+    res.render("html/room.html")
 });
 
 app.listen(port);
