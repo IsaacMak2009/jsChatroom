f7a9235d6c40b72ff4001969c636468d54fe2973
IsaacMak2009 <95964568+IsaacMak2009@users.noreply.github.com>
2022-07-10 14:30:12 +0800

    changed somethings

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 // import libraries
 const express = require('express');
-const path = require('path');
 const config = require('config');
-
 var app = express();
+
+// load configuration
 var host = config.get('server.host');
 var port = config.get('server.port');
+var verify = config.get('app.verify');
+var verifycode = verify ? config.get('app.verify-code'):null;
+
 
 // use ejs as view engine
-app.set('view engine', 'ejs');
-app.set("view", path.join(__dirname, 'public', 'html'));
+app.set("views", __dirname+'/public/');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // set static path
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.send("Hello World!");
+    res.render('html/login.html', {
+        isverify: verify,
+        code: verifycode
+    });
 });
 
 app.listen(port);
